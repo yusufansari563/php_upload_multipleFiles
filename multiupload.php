5e9134aead735d2aac82f41f9fd10a8eb302ecde
yusufansari563 <yusufansari563@gmail.com>
2021-01-05 13:16:59 +0530

    first commit

---
+++ multiupload.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('db.php');
+if(isset($_POST['submit_image']))
+{
+    $images_array=array();
+     foreach($_FILES['images']['name'] as $key=>$val){
+    
+     $uploadfile=$_FILES["images"]["tmp_name"][$key];
+     $folder="images/";
+     $target_file = $folder.$_FILES['images']['name'][$key];
+     if(move_uploaded_file($_FILES["images"]["tmp_name"][$key], "$folder".$_FILES["images"]["name"][$key])){
+        $sql = "INSERT INTO imageuploadtest VALUES ('.$target_file.')"; 
+        // Execute query 
+        mysqli_query($conn, $sql);
+         $images_array[] = $target_file;
+     }
+    }
+}
+if(!empty($images_array)){ 
+    foreach($images_array as $src){ ?>
+        <ul>
+            <li >
+                <img src="<?php echo $src; ?>">
+            </li>
+        </ul>
+<?php }
+}
+?>
